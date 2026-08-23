719e6dea6d4bd7cd7bba4828967a05404f0dab49
arian <70283699+arian-gif@users.noreply.github.com>
2026-08-23 12:02:57 -0400

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # hash map prefixsum: count 
+        sub_array = {
+            0:1
+        }
+
+        curr, res= 0,0
+
+        for n in nums:
+            curr +=n
+            if (curr - k) in sub_array:
+                res+= sub_array.get(curr-k,0)
+            sub_array[curr] = sub_array.get(curr,0)+1 
+        return res
+        
+        
+        
